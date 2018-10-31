e3ae57ec8e27e65c63946cb7f8eacb429b4abbba
github username <github email address>
2018-10-31 11:06:36 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,54 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
-  get '/' do
+  # get '/' do
+  #   erb :inde
+  # end
 
+  get '/posts' do
+  #pass through to controller(index.erb) to iterate through
+  @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    name = params[:name]
+    content = params[:content]
+    @post = Post.create(name: name, content: content)
+      redirect "/posts"
+  end
+
+  get '/posts/:id' do
+    post_id = params[:id]
+    @post = Post.find(post_id)
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    #grab book youre trying to edit
+
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name],
+                content: params[:content])
+    redirect "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.destroy
+      redirect "/posts"
   end
 end
