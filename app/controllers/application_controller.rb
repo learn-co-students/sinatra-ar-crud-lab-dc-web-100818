38ad48407706e204c6b86827c57f2076fa945184
Robin Tram <robin.tram@gmail.com>
2018-10-31 10:44:36 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-
 require_relative '../../config/environment'
+require "pry"
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +9,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
+  end
 
+  get "/posts/new" do
+    erb :new
+  end
+
+  get "/posts" do
+    @posts = Post.all
+    erb :index
+  end
+
+  post "/posts" do
+    # binding.pry
+    Post.create(params)
+    # @posts = Post.all
+    erb :index
+  end
+
+  get "/posts/:id" do
+    @post = Post.find(params[:id])
+    # binding.pry
+    erb :show
+  end
+
+  get "/posts/:id/edit" do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch "/posts/:id" do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete "/posts/:id/delete" do
+    # binding.pry
+    Post.destroy(params[:id])
   end
 end
